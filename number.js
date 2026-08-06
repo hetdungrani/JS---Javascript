bab8afdb2913c41f17b82184ebcea564c89c74cd
het <hetdunranihet930@gmail.com>
2026-08-06 19:54:03 +0530

    number

---
+++ number.js
@@ -0,0 +1,15 @@
+
+let balance = 100.23
+console.log(balance.toFixed(1)); // 100.2
+
+
+console.log(Math)
+console.log(Math.abs(-4) )// for positive value => 4
+console.log(Math.round(2.3)) // for final value => 2
+console.log(Math.ceil(4.7) )// highest number => 5
+console.log(Math.floor(4.3) )// lowest number => 4
+console.log(Math.min(4,5,6,7)) // 4
+console.log(Math.max(4,7,8,4) )// 8
+console.log(Math.random() )// random value from 0 to 1
+console.log(Math.floor(Math.random()* 100))
+
