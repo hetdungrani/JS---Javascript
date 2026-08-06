81ab933036c45489d097ba592ac127b670fb49a3
het <hetdunranihet930@gmail.com>
2026-08-06 17:21:46 +0530

    comparision and conversion between data and datatypes

---
+++ comparision.js
@@ -0,0 +1,19 @@
+console.log(1 == 2);
+console.log(1 <= 2);
+console.log(1 >= 3);
+console.log(1 != 3);
+console.log(1 === 2);
+console.log(1 < 2);
+console.log(1 > 3);
+
+console.log("1"+ 2);
+console.log(3 + "2");
+
+console.log(undefined == 0);
+console.log(undefined <= 0);
+console.log(undefined >= 0);
+
+console.log(null <= 0);
+console.log(null == 0);
+console.log(null > 0);
+
---
+++ conversion.js
@@ -0,0 +1,9 @@
+let marks="60abc";
+
+console.log(marks);
+console.log(typeof marks)
+
+let submarks=Number(marks)
+
+console.log(submarks)
+console.log(typeof submarks)
