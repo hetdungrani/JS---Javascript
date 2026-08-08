eb8afc1c585700b33e0dc090c13d31aa6ea1c893
het <hetdunranihet930@gmail.com>
2026-08-08 15:36:00 +0530

    add declaration examples

--- function.js
+++ function.js
@@ -14,12 +14,31 @@ function sumOfTwoNumbers(n1, n2) {
 }
 
 // sumOfTwoNumbers(5, 10);
-const sumnumber= sumOfTwoNumbers(5, 10);
+const sumnumber = sumOfTwoNumbers(5, 10);
 // console.log(sumnumber)
 
-function cartPrices(...prices){  // without ... it will take only first value and ignore the rest
-    return prices
+function cartPrices(...prices) {
+  // without ... it will take only first value and ignore the rest
+  return prices;
 }
 
 // const cart = [100, 200, 300, 400, 500];
-console.log(cartPrices(100, 200, 300, 400, 500)); 
+// console.log(cartPrices(100, 200, 300, 400, 500));
+
+// arrow function
+// add(1,2) // not allow before decration
+const add = (n1, n2) => {
+  return n1 + n2;
+};
+
+// add2(1,2) // not allow before decration
+const add2 = function (n1, n2) {
+  return n1 + n2;
+};
+
+// myName2(); // allow before decration
+function myName2() {
+  console.log("H");
+  console.log("E");
+  console.log("T");
+}
