eed0e9eb36e5e5055a6f2ddc09f1f080121cc185
het <hetdunranihet930@gmail.com>
2026-08-10 10:59:47 +0530

    ifelse and switch with notes

---
+++ if_else.js
@@ -0,0 +1,19 @@
+let userLoggedIn = true;
+let debitCardActive = false;
+let userLoggedInGoogle = true;
+let userLoggedInFacebook = false;
+
+// if (userLoggedIn) {
+//     console.log("User is logged in");
+// }
+
+// if (userLoggedIn && debitCardActive) {
+//     console.log("User is ture");
+// }else {
+//     console.log("User is false");
+// }
+
+if (userLoggedInGoogle || userLoggedInFacebook) {
+    console.log("User is logged in");
+}
+
